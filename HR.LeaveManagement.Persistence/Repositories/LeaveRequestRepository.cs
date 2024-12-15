fec2d4fbde972f615262c7df8b2903d5517a0694
Mahdi Morovati <morovati155@gmail.com>
2024-12-15 07:56:50 +0330

    implement repositories

--- HR.LeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
+++ HR.LeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
@@ -1,6 +1,7 @@
 using HR.LeaveManagement.Application.Contracts.Persistence;
 using HR.LeaveManagement.Domain;
 using HR.LeaveManagement.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
 
 namespace HR.LeaveManagement.Persistence.Repositories;
 
@@ -8,5 +9,28 @@ public class LeaveRequestRepository : GenericRepository<LeaveRequest>, ILeaveReq
 {
     public LeaveRequestRepository(HrDatabaseContext context) : base(context)
     {
+    }
+
+    public async Task<LeaveRequest?> GetLeaveRequestWithDetails(int id)
+    {
+        return await Context.LeaveRequests
+            .Include(q => q.LeaveType)
+            .FirstOrDefaultAsync(q => q.Id == id);
+
+    }
+
+    public async Task<List<LeaveRequest>> GetLeaveRequestsWithDetails()
+    {
+        return await Context.LeaveRequests
+            .Include(q => q.LeaveType)
+            .ToListAsync();
+    }
+
+    public async Task<List<LeaveRequest>> GetLeaveRequestsWithDetails(string userId)
+    {
+        return await Context.LeaveRequests
+            .Where(q => q.RequestingEmployeeId == userId)
+            .Include(q => q.LeaveType)
+            .ToListAsync();
     }
 }
