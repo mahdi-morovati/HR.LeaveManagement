842c534b9a4c76ba7a65599793d89fca09acbcc0
Mahdi Morovati <morovati155@gmail.com>
2024-12-18 14:04:59 +0330

    create GetLeaveRequestList query

---
+++ HR.LeaveManagement.Application/Models/Identity/Employee.cs
@@ -0,0 +1,9 @@
+namespace HR.LeaveManagement.Application.Models.Identity;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
