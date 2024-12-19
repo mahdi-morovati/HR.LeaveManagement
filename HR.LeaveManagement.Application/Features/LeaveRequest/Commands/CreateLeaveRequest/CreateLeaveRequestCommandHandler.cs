021e9f0b6162be154867fbfa919298773c1693ec
Mahdi Morovati <morovati155@gmail.com>
2024-12-19 07:47:10 +0330

    implement CreateLeaveRequestCommandHandler

--- HR.LeaveManagement.Application/Features/LeaveRequest/Commands/CreateLeaveRequest/CreateLeaveRequestCommandHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveRequest/Commands/CreateLeaveRequest/CreateLeaveRequestCommandHandler.cs
@@ -1,6 +1,42 @@
+using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
+using MediatR;
+
 namespace HR.LeaveManagement.Application.Features.LeaveRequest.Commands.CreateLeaveRequest;
 
-public class CreateLeaveRequestCommandHandler
+public class CreateLeaveRequestCommandHandler : IRequestHandler<CreateLeaveRequestCommand, Unit>
 {
-    
+    private readonly ILeaveRequestRepository _leaveRequestRepository;
+    private readonly IMapper _mapper;
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+    public CreateLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository, IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+    {
+        _leaveRequestRepository = leaveRequestRepository;
+        _mapper = mapper;
+        _leaveTypeRepository = leaveTypeRepository;
+    }
+    public async Task<Unit> Handle(CreateLeaveRequestCommand request, CancellationToken cancellationToken)
+    {
+        // validate
+        var validator = new CreateLeaveRequestCommandValidator(_leaveTypeRepository);
+        var validationResult = await validator.ValidateAsync(request);
+
+        if (validationResult.Errors.Any())
+        {
+            throw new BadRequestException("Invalid Leave Request", validationResult);
+        }
+
+        // get requesting employee's id
+        // check on employee's allocation
+        // if allocations aren't enough, return validation error with message
+        
+        // create leave request
+        var leaveRequest = _mapper.Map<Domain.LeaveRequest>(request);
+        await _leaveRequestRepository.CreateAsync(leaveRequest);
+        
+        // send confirmation email
+
+        return Unit.Value;
+    }
 }
