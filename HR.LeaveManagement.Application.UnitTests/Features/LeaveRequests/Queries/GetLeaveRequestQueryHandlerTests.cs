c290e081f7807313ea0a84f2b81b218e13bf6b29
Mahdi Morovati <morovati155@gmail.com>
2024-12-24 14:54:30 +0330

    create GetLeaveRequestQueryHandlerTests.Handle_LeaveRequestFound_ReturnsLeaveRequestDetailsDto

--- HR.LeaveManagement.Application.UnitTests/Features/LeaveRequests/Queries/GetLeaveRequestQueryHandlerTests.cs
+++ HR.LeaveManagement.Application.UnitTests/Features/LeaveRequests/Queries/GetLeaveRequestQueryHandlerTests.cs
@@ -1,9 +1,11 @@
 using AutoMapper;
 using HR.LeaveManagement.Application.Contracts.Logging;
 using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Features.LeaveRequest.Queries.GetLeaveRequestDetail;
 using HR.LeaveManagement.Application.Features.LeaveRequest.Queries.GetLeaveRequestList;
 using HR.LeaveManagement.Application.MappingProfiles;
 using HR.LeaveManagement.Application.UnitTests.Mocks;
+using HR.LeaveManagement.Domain;
 using Moq;
 using Shouldly;
 using Xunit;
@@ -31,7 +33,7 @@ public class GetLeaveRequestQueryHandlerTests
 
 
     [Fact]
-    public async Task GetLeaveRequestListTest()
+    public async Task GetLeaveRequestsWithDetailsTest()
     {
         var handler = new GetLeaveRequestListQueryHandler(_mockRepo.Object, _mapper);
 
@@ -40,5 +42,41 @@ public class GetLeaveRequestQueryHandlerTests
         result.ShouldBeOfType<List<LeaveRequestListDto>>();
         result.Count.ShouldBe(3);
     }
-    
+
+    [Fact]
+    public async Task Handle_LeaveRequestFound_ReturnsLeaveRequestDetailsDto()
+    {
+        // Arrange
+        var leaveRequestId = 1;
+        var leaveRequest = new LeaveRequest
+        {
+            Id = leaveRequestId,
+            RequestingEmployeeId = "123",
+            LeaveTypeId = 1,
+            LeaveType = new LeaveType
+            {
+                Id = 1,
+                Name = "Vacation",
+                DefaultDays = 10
+            },
+            StartDate = DateTime.Now,
+            EndDate = DateTime.Now.AddDays(5)
+        };
+        _mockRepo.Setup(r => r.GetLeaveRequestWithDetails(leaveRequestId))
+            .ReturnsAsync(leaveRequest);
+        
+        var handler = new GetLeaveRequestDetailQueryHandler(_mockRepo.Object, _mapper);
+         
+        
+        // Act
+        var result = await handler.Handle(new GetLeaveRequestDetailQuery {Id = leaveRequestId}, CancellationToken.None);
+        
+        // Assert
+        result.ShouldNotBeNull();
+        result.Id.ShouldBe(leaveRequestId);
+        result.LeaveType.Name.ShouldBe(leaveRequest.LeaveType.Name);
+        result.RequestingEmployeeId.ShouldBe(leaveRequest.RequestingEmployeeId);
+
+    }
+
 }
--- HR.LeaveManagement.Application/Features/LeaveRequest/Queries/GetLeaveRequestDetail/LeaveRequestDetailsDto.cs
+++ HR.LeaveManagement.Application/Features/LeaveRequest/Queries/GetLeaveRequestDetail/LeaveRequestDetailsDto.cs
@@ -5,7 +5,7 @@ namespace HR.LeaveManagement.Application.Features.LeaveRequest.Queries.GetLeaveR
 
 public class LeaveRequestDetailsDto
 {
-    public int id { get; set; }
+    public int Id { get; set; }
     public Employee Employee { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
