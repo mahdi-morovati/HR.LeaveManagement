7b49596b98e7c5e55b8762e9ad7ff949615849ea
mahdi-morovati <morovati155@gmail.com>
2025-01-30 13:40:13 +0330

    create Http Client with NSwag, create services and interfaces

--- HR.LeaveManagement.BlazorUI/Program.cs
+++ HR.LeaveManagement.BlazorUI/Program.cs
@@ -1,11 +1,22 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using HR.LeaveManagement.BlazorUI;
+using HR.LeaveManagement.BlazorUI.Contracts;
+using HR.LeaveManagement.BlazorUI.Services;
+using HR.LeaveManagement.BlazorUI.Services.Base;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Microsoft.Extensions.Http
+builder.Services.AddHttpClient<IClient, Client>(client =>
+    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+
+builder.Services.AddScoped<ILeaveTypeService, LeaveTypeService>();
+builder.Services.AddScoped<ILeaveAllocationService, ILeaveAllocationService>();
+builder.Services.AddScoped<ILeaveRequestService, ILeaveRequestService>();
 
 await builder.Build().RunAsync();
---
+++ HR.LeaveManagement.BlazorUI/Services/Base/BaseHttpService.cs
@@ -0,0 +1,11 @@
+﻿namespace HR.LeaveManagement.BlazorUI.Services.Base;
+
+public class BaseHttpService
+{
+    protected IClient Client;
+
+    public BaseHttpService(IClient client)
+    {
+        Client = client;
+    }
+}
