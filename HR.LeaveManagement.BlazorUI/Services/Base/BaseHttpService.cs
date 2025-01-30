0e1448df1eccd145e58b68c99f789f513d31e419
mahdi morovati <morovati155@gmail.com>
2025-01-30 19:18:38 +0330

    create BaseHttpService.ConvertApiException<Guid>, create Response

--- HR.LeaveManagement.BlazorUI/Services/Base/BaseHttpService.cs
+++ HR.LeaveManagement.BlazorUI/Services/Base/BaseHttpService.cs
@@ -1,4 +1,6 @@
-﻿namespace HR.LeaveManagement.BlazorUI.Services.Base;
+﻿using HR.LeaveManagement.BlazorUI.Pages;
+
+namespace HR.LeaveManagement.BlazorUI.Services.Base;
 
 public class BaseHttpService
 {
@@ -7,5 +9,32 @@ public class BaseHttpService
     public BaseHttpService(IClient client)
     {
         Client = client;
+    }
+
+    protected Response<Guid> ConvertApiException<Guid>(ApiException ex)
+    {
+        switch (ex.StatusCode)
+        {
+            case 400:
+                return new Response<Guid>
+                {
+                    Message = "Invalid data was submitted",
+                    ValidationErrors = ex.Response,
+                    Success = false
+                };
+
+            case 404:
+                return new Response<Guid>
+                {
+                    Message = "The record was not found",
+                    Success = false
+                };
+            default:
+                return new Response<Guid>
+                {
+                    Message = "Something went wrong, please try again later.",
+                    Success = false
+                };
+        }
     }
 }
