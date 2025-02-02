e931d6529d220113ab8cd96db0aa40d32c0cbfff
mahdi-morovati <morovati155@gmail.com>
2025-02-02 10:43:00 +0330

    create user login, register services

--- HR.LeaveManagement.Application/Models/Identity/Employee.cs
+++ HR.LeaveManagement.Application/Models/Identity/Employee.cs
@@ -2,7 +2,7 @@ namespace HR.LeaveManagement.Application.Models.Identity;
 
 public class Employee
 {
-    public int Id { get; set; }
+    public string Id { get; set; }
     public string Email { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
